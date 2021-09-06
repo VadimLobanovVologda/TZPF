29bb6962f0e82fbf8761cd55df790cce2dcfefa9
VadimLobanovVologda <Badirn@yandex.ru>
2021-09-06 03:59:23 +0300

    start

---
+++ src/components/app.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from "./app.scss"
+
+const App = () => {
+  return <div className={styles.app}>text</div>
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,91 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.jsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[hash].js',
+  },
+  resolve: {
+    extensions: ['.jsx', '.js', '.scss', '.css'],
+    alias: {
+      images: path.resolve('src/assets/images'),
+      actions: path.resolve('src/actions'),
+      styles: path.resolve('src/assets/styles'),
+      helpers: path.resolve('src/helpers'),
+      components: path.resolve('src/components'),
+      node_modules: path.resolve('src/node_modules'),
+      data: path.resolve('src/assets/data'),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react', '@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /.(css|scss)$/,
+        exclude: /node_modules/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: {
+                localIdentName: '[name]_[local]_[hash:base64:3]',
+              },
+            },
+          },
+          'postcss-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        exclude: /node_modules/,
+        loader: 'file-loader',
+        options: {
+          name: 'images/[path][name].[ext]',
+        },
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        exclude: /node_modules/,
+        loader: 'file-loader',
+        options: {
+          name: '[path][name].[ext]',
+        },
+      },
+    ],
+  },
+  devServer: {
+    port: 3000,
+    hot: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+    new CleanWebpackPlugin(),
+  ],
+};
